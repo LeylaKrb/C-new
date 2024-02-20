6493724cc7e3fe18c652c6fdd8fff0c523a765a9
LeylaKrb <142938363+LeylaKrb@users.noreply.github.com>
2024-02-20 23:51:15 +0300

    Add files via upload

---
+++ Entities/Course.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro.Entities;
+
+
+
+public class Course //her bir kursta bu 4 veriyi bulundurucak//sınıfımız ve alt başlıklarda bu sınıfın özellilkleri
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public double Price {  get; set; }
+
+
+}
